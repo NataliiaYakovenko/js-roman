9ffab7999872bcb0b2ab1ec659c2ff427ef53ed7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-11-05 22:44:12 +0200

    Add const

--- index.js
+++ index.js
@@ -1,41 +1,14 @@
+let box1 = 'Hello world!';
 
+let box2;
 
-let box;
-
-console.log(box);
-
-box = 3.9995;
-
-console.log(box);
-
-let box2 = 'Hello, world';
-
-console.log(box2);
-
-box2 = 236;
+box2 = 15;
 
 console.log(box2)
 
-let isActive = true;
+console.log(box1)
 
-let isLogged = false;
+const birthday = 'octovber';
 
-let isRecording = false;
+console.log(birthday);
 
-isRecording = true;
-
-let x = 10;
-
-let y = x;
-
-console.log(y);
-
-let z = 10;
-
-let u = z;
-
-u = 15;
-
-console.log(z);
-
-console.log(u);
