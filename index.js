7a1c17b1d6b860ee7d3b5bc288f842ba349e2cd3
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-11-08 21:01:24 +0200

    Add logocal operators

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-let k = 0; 
-// Збільшити значення к на 3
-k = k + 3;  //Перевизначаємо к
-console.log(k);
 
-// Зменшити значення к на 2
-k = k - 2;  //Перевизначаємо к
-console.log(k);
+// Логічні оператори
+const a = true;
+console.log(a); //true
 
-//Інший спосіб
-// Збільшити значення к на 3
-k += 3;
-console.log(k);
+// ! - заперечення
+!a;
+console.log(!a); //false
 
-// Зменшити значення к на 2
-k -= 2;
-console.log(k);
+// !! - подвійне заперечення
+!!a;
+console.log(!!a); //true
 
-// Помножити значення к на 3
-k *= 3;
-console.log(k);
-
-// Поділити значення к на 2
-k /= 2;
-console.log(k)
-
-//Унарні оператори
--k;
-+k;
-
-// '+' як оператор приведення строки до числа
-//let thisIsNumber = '455'; це string type
-let thisIsNumber = Number('455'); //змінився на number type
-console.log(typeof thisIsNumber);
-
+//або
+//явне приведення
+console.log(Boolean(0));//false
+console.log(Boolean(1));//true
