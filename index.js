45dedd8c23d22565339efac922898bc065b953ad
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-06 22:39:01 +0200

    Add FizzBuzz + Задачі

--- index.js
+++ index.js
@@ -1,41 +1,73 @@
 //ЗАДАЧІ
 
-// for(початковий стан, умова за якої лічильник повторюється,зміна лічильника на кожній інтерації){
-//  блок коду, який повторюється} 
-
-
-
-//1 Написати функцію, яка обчислює факторіал переданого їй числа,
-//результат роботи функції вивести на консоль
-
-//Факторіал це
-//5!=1*2*3*4*5 = 120
-//10! = 1*2*3*4*5*6*7*8*9*10
-
-//Якщо число від'ємне - поверніть з функції помилку
-//Якщо число = 0, або число = 1 - поверніть з функції 1
-//Знаходимо факторіал...
-
-function factorial(number){
-    let res = 1; // у цій змінній ми будемо накопичувати добуток
-
-    if (number === 0 || number === 1){
-        return 1;
-    } else if (number<0){
-        return null
-    } 
-
-    for(let it = 1; it <= number; it++){
-        res = res * it
+/*
+1. Написати функцію, яка приймає два числа (діапазон) 
+і виводить на консоль всі числа з цього діапазону, які діляться на 5
+*/
+//Варіант1
+function printNumberDivisibleBy5(start, end) {
+  for (let i = start; i <= end; i++) {
+    if (i % 5 === 0) {
+      console.log(i);
     }
-    return res;
+  }
 }
-console.log(factorial);
+printNumberDivisibleBy5(10, 50);
 
+//Варіант2
 
-//2. Вивести 10 консольлогів
+function count() {
+  let numberOne = Number(prompt("Enter number one"));
+  let numberTwo = Number(prompt("Enter number two"));
 
-for(let i = 1; i < 10; i = i + 3){
-    //0 -> 3 -> 6 -> 9 -> 12 (false)
-    console.log(i);
+  for (let it = numberOne; it <= numberTwo; it++) {
+    if (it % 5 === 0) {
+      console.log(it);
+    } else {
+      console.log("Number " + it + " There is not number % 5");
+    }
+  }
 }
+count()
+
+//Варіант3 Максимально підходящий
+function count() {
+    let numberOne = Number(prompt("Enter number one"));
+    let numberTwo = Number(prompt("Enter number two"));
+    if(numberOne > numberTwo){
+        console.log('Початкове число повинно бути меншим за кінцеве число');
+        return; // достроковий вихід із циклу
+    }
+  
+    for (let it = numberOne; it <= numberTwo; it++) {
+      if (it % 5 === 0) {
+        console.log(it);
+      } else {
+        console.log("Number " + it + " There is not number % 5");
+      }
+    }
+  }
+  count()
+
+  //ЗАДАЧА   FizzBuzz - часто на спвбеседі
+  // Написати гру FizzBuzz для 100 чисел
+  //Починаємо з 1, і потім це число поступово збільшується інкрементується
+  //Якщо число ділеться на 3, то гравець каже Fizz
+   //Якщо число ділеться на 5, то гравець каже Buzz
+   //Якщо число ділеться і на 3 і на 5, то гравець каже FizzBuzz
+   //Якщо число ділеться ні на 3 ні на 5, то гравець називає це число
+
+   function writeGameFor100Number(limit){
+    for(let i = 1; i <= limit; i++){
+        if(i %3 === 0  && i % 5 === 0){
+            console.log('FizzBuzz');
+        }else if(i % 3 === 0){
+            console.log('Fizz');
+        }else if(i % 5 === 0){
+            console.log('Buzz');
+        }else{
+            console.log(i)
+        }
+    }
+   }
+   writeGameFor100Number(100)
