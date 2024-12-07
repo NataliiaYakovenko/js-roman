79f1d6b740830bb06f8e66eac726d64eefec09c6
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-07 20:27:52 +0200

    Add object

--- index.js
+++ index.js
@@ -1,73 +1,47 @@
-//ЗАДАЧІ
+//ОБ'ЄКТИ
 
 /*
-1. Написати функцію, яка приймає два числа (діапазон) 
-і виводить на консоль всі числа з цього діапазону, які діляться на 5
+Об'єкт
+
+const назаваОб'єкту = {
+   ключ: значення
+}
+Об'єкт мають:
+ - властивості
+ - методи
 */
-//Варіант1
-function printNumberDivisibleBy5(start, end) {
-  for (let i = start; i <= end; i++) {
-    if (i % 5 === 0) {
-      console.log(i);
-    }
+
+const obj = {
+  color: 'white',
+  fontSize: 2,
+}
+
+// ООП (об'єктно орієнтовне програмування)
+// ООП - парадигрма програмування, яка дозволяє створювати програми
+// з використанням об'єктів, які маю властивості і методи
+
+//Задача:Опишіть об'єкт лампочки
+const bulb = {
+  lightness: 200, // яскравість
+  power:"200W",   //  потужність
+  cap: 'small',    //  цоколь
+  //оголошення методу
+  on: function(){
+    return 'The light on!';
+  },
+  off: function(){
+     return 'The light off!';
   }
 }
-printNumberDivisibleBy5(10, 50);
+console.log(bulb.power);
+console.log(bulb.off())
+//______________________________
 
-//Варіант2
+//Задача
+const dog ={
+  name: 'Tusik',
+  'favorite food': 'meat',
 
-function count() {
-  let numberOne = Number(prompt("Enter number one"));
-  let numberTwo = Number(prompt("Enter number two"));
-
-  for (let it = numberOne; it <= numberTwo; it++) {
-    if (it % 5 === 0) {
-      console.log(it);
-    } else {
-      console.log("Number " + it + " There is not number % 5");
-    }
-  }
 }
-count()
-
-//Варіант3 Максимально підходящий
-function count() {
-    let numberOne = Number(prompt("Enter number one"));
-    let numberTwo = Number(prompt("Enter number two"));
-    if(numberOne > numberTwo){
-        console.log('Початкове число повинно бути меншим за кінцеве число');
-        return; // достроковий вихід із циклу
-    }
-  
-    for (let it = numberOne; it <= numberTwo; it++) {
-      if (it % 5 === 0) {
-        console.log(it);
-      } else {
-        console.log("Number " + it + " There is not number % 5");
-      }
-    }
-  }
-  count()
-
-  //ЗАДАЧА   FizzBuzz - часто на спвбеседі
-  // Написати гру FizzBuzz для 100 чисел
-  //Починаємо з 1, і потім це число поступово збільшується інкрементується
-  //Якщо число ділеться на 3, то гравець каже Fizz
-   //Якщо число ділеться на 5, то гравець каже Buzz
-   //Якщо число ділеться і на 3 і на 5, то гравець каже FizzBuzz
-   //Якщо число ділеться ні на 3 ні на 5, то гравець називає це число
-
-   function writeGameFor100Number(limit){
-    for(let i = 1; i <= limit; i++){
-        if(i %3 === 0  && i % 5 === 0){
-            console.log('FizzBuzz');
-        }else if(i % 3 === 0){
-            console.log('Fizz');
-        }else if(i % 5 === 0){
-            console.log('Buzz');
-        }else{
-            console.log(i)
-        }
-    }
-   }
-   writeGameFor100Number(100)
+console.log(dog.name);
+console.log(dog['favorite food']);
