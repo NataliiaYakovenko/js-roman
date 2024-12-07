b1ccfa7d3502bf6812231e3cc526d3123b91979d
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-07 21:20:20 +0200

    Add способи створення об'єктів

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
-//ОБ'ЄКТИ
+//Способи створення об'єктів
 
-/*
-Об'єкт
-
-const назаваОб'єкту = {
-   ключ: значення
-}
-Об'єкт мають:
- - властивості
- - методи
-*/
-
-const obj = {
-  color: 'white',
-  fontSize: 2,
+//1. Літеральне створення об'єкта
+const cat1 = {
 }
 
-// ООП (об'єктно орієнтовне програмування)
-// ООП - парадигрма програмування, яка дозволяє створювати програми
-// з використанням об'єктів, які маю властивості і методи
+//2. Функція конструктор для об'єкта
+const cat2 = Object();
 
-//Задача:Опишіть об'єкт лампочки
-const bulb = {
-  lightness: 200, // яскравість
-  power:"200W",   //  потужність
-  cap: 'small',    //  цоколь
-  //оголошення методу
-  on: function(){
-    return 'The light on!';
+//3. Функція конструктор для об'єкта з new
+const cat3 = new Object();
+//__________________________________________
+
+//Задача. Створити об'єкт кота
+const cat = {
+  name:'Mursik',
+  color:'Red',
+  breed:'Dvoroviy',
+  age: 2,
+  eat: function(){
+    return 'I am eating';
   },
-  off: function(){
-     return 'The light off!';
+  sleep: function(){
+    return ' I am sleeping';
   }
 }
-console.log(bulb.power);
-console.log(bulb.off())
-//______________________________
-
-//Задача
-const dog ={
-  name: 'Tusik',
-  'favorite food': 'meat',
-
+//Нам потрібно в об'єкті змінити вік кота з 2 до 3 років
+cat.age = cat. age + 1;
+console.log(cat.age);
+//Нам потрібно видалити з об'єкта якусь властивість (наприклад метод sleep)
+delete cat.sleep;
+//Нам потрібно додати в об'єкт якусь властивість, використовується крапка .
+//Додати ім'я друга кота Мурзика до об'єкту кота Мурзика
+cat.friend = 'Tusik';
+console.log(cat.friend);
+//Додати улюблену іжу кота
+cat.favoriteFood = 'fish';
+console.log(cat.favoriteFood);
+//Додати метод, що кіт стрибає
+cat.jump = function(){
+  return 'I am jumping';
 }
-console.log(dog.name);
-console.log(dog['favorite food']);
+console.log(cat.jump);
