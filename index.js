c9770b624eee4a63836ec305ce2300d4470010a4
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-12 17:36:36 +0200

    Add задача книжковий магазин

--- index.js
+++ index.js
@@ -1,19 +1,65 @@
+/*Розробка програми для керуванням книжковим магазином
+1, потрібно створити конструктор об'єктів книг з наступними властивостями
+Спроектувати методи для отримання і всановлення значень цих властивостей
+2, Зробити для всіх книг метод, який би повернув ціну книжки зі знижкою
 
-//Задача функція - конструктор
-/*Створити функцію-конструктор собаки, у якого має бути ім'я, колір, вік
-  Зробіть декілька собак
 */
-function Dog(name, color,age){
-  this.name = name;
-  this.color = color;
-  this.age = age;
-  this.gav = function(){
-    return this.name +' is gav-gav-gav';
+function Book(title, auther,year, price){
+  this.title = title;
+  this.auther = auther;
+  this.year = year;
+  this.price = price;
+  
+  this.getTitle = function() {
+    return this.title;
+  };
+  this.getAuther = function(){
+    return this.auther;
+  };
+  this.getYear = function(){
+    return this.year;
+  };
+  this.getPrice = function(){
+    return this.price;
+  };
+  //змінити назву книги
+  this.setTitle = function (newTitle) {
+    //також можна реалізуваи якісь перевірки
+    this.title = newTitle;
   }
-}
-const dog1 = new Dog('Archy','grey',3);
-console.log(dog1);
-const dog2 = new Dog('Busia',null,2);
-console.log(dog2);
-const dog3 = new Dog('Jerry','black',null);
-console.log(dog3);
+    //змінити автора книги
+  this.setAuther = function(newAuther){
+       //також можна реалізуваи якісь перевірки
+    this.auther = newAuther;
+  }
+    //змінити рік книги
+  this.setYear = function(newYear){
+     //також можна реалізуваи якісь перевірки
+    this.year = newYear;
+  }
+    //змінити ціну книги
+  this.setPrice = function(newPrice){
+     //також можна реалізуваи якісь перевірки
+    this.price = newPrice;
+  }
+  //метод, який би повернув ціну книжки зі знижкою
+  this.culculateDiscountedPrice = function(discountPercentage){
+const discountPrice = this.price - (this.price*(discountPercentage/100))
+return discountPrice;
+  }
+};
+
+const book1 = new Book('Чотири вітри','Крістін Геннан',2020,450);
+/*console.log(book1.getTitel());
+console.log(book1.getAuther());
+console.log(book1.getYear());
+console.log(book1.getPrice());
+
+book1.setTitle('New Title');
+book1.setAuther('New Auther');
+book1.setYear(2025);
+book1.setPrice(350);*/
+
+
+//повернув ціну книжки зі знижкою
+console.log(book1.culculateDiscountedPrice(10));
