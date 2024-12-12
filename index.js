6cfd9412703d8f91208fcc30507b70fe77883bc3
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-12 23:17:42 +0200

    Задача об'єкт

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// Задача. у нас є об'єкт з різними версіями привітань для користувача
-//Вивести користувачу привітання взалежності від значення, яке він ввів
- const greets ={
-    15: 'Hi',
-    17: 'Ya, whats up',
-    19: 'Hello',
-    30: 'Good morning',
- }
- const valueFromUser = 19;
+// Задача. у нас є об'єкт ключем в якому є день тижня, а значенням плаихни на цей день
+//прийняти через промпт від корстувача назву дня тижня 
+// і виадати у альорт, що заплановано у користувача на цей тиждень
+const obj = {
+    Non:'drive lessons',
+    Tue: 'dantist visit',
+    Wed: 'go to party',
+    Thu: 'hard work',
+    Fri: 'rest',
+    Sat: 'go to park',
+}
+const nameWeek = prompt('enter week day ( \n Monday = Mon, \n Tuesday = Tue, \n Wed, \n Thu, \n Fri, \n Sat )')
+alert(`On ${nameWeek} you have ${obj[nameWeek]}`)
 
- //console.log(greets[19]);
- console.log(greets[valueFromUser]);
+//alert(obj[nameWeek] ? obj[nameWeek] : 'Error happened') - зробити через тернальний оператор
+
