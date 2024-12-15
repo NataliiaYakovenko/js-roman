bd1135dfffe94ead2aa3ec2f28cd6ae51f73f0c4
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-15 22:10:29 +0200

    Add задача з map

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-// Задача 1 forEach 
-// Дано масив об'єктів
-//Всім користувачам збільшити вік на 1
-
-const users = [{
-    name:'Nataliia',
-    lastName:'Yakovenko',
-    age: 41,
-    email: 'yakovenkonatali999@gmail.com',
-},{
-    name:'Roland',
-    lastName:'Simonyan',
-    age: 26,
-    email: 'roland@gmail.com',
-},{
-    name:'Bogdan',
-    lastName:'Horobriy',
-    age: 30,
-    email: 'horobriy@gmail.com',
-}]
-//Варіант1 мій
-users.forEach((item) => item.age += 1)
-console.log(users);
-
-//Варіант2 Рома
-function plusOneYear(item){
-    item.age +=1;
+// Задача 2 map
+// Дано масив 
+//Зробити новий масив, де всі елементи якого = елемент зі сторого масиву + 100
+//variant 1
+const array = [2,44,11,234,8,2,4,1]
+function items(number){
+    return number + 100;
 }
-users.forEach(plusOneYear)
+const newArray = array.map(items);
+console.log(newArray);
+
+//variant 1 стрілочна функція
+const newArray1 = array.map ((item)=>item+100);
