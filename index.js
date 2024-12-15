950a14ee60a8d7e50c3e341b72d0258bbb7973f7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-15 21:49:01 +0200

    Add задача з forEach

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-// МЕТОДИ ОБХОДУ МАСИВА 
-// map 
-//array.map - працює як forEach (викликає функцію зворотнього виклику), тільки map буде повертати(створювати) новий масив
-const array = [1,2,3,4,5,]
-function square(number){
-    return number * number;
+// Задача 1 forEach 
+// Дано масив об'єктів
+//Всім користувачам збільшити вік на 1
+
+const users = [{
+    name:'Nataliia',
+    lastName:'Yakovenko',
+    age: 41,
+    email: 'yakovenkonatali999@gmail.com',
+},{
+    name:'Roland',
+    lastName:'Simonyan',
+    age: 26,
+    email: 'roland@gmail.com',
+},{
+    name:'Bogdan',
+    lastName:'Horobriy',
+    age: 30,
+    email: 'horobriy@gmail.com',
+}]
+//Варіант1 мій
+users.forEach((item) => item.age += 1)
+console.log(users);
+
+//Варіант2 Рома
+function plusOneYear(item){
+    item.age +=1;
 }
-const newArray = array.map(square);
-console.log(newArray);
-
-//можна вирішити цю задачу через стрілкову функцію
-const newArray1 = array.map((item)=>{
-    return item * item;
-})
-console.log(newArray1);
-
-// Коли ми використовуємо map, в такому випдку, значення, яке ви повертаєте (return) з calback - воно і буде входити у результуючий масив
+users.forEach(plusOneYear)
