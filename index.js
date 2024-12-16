9531ce5bcc36ae640f552e7a1939df7125acd98b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-16 23:56:03 +0200

    Add об'єкт функції Arguments

--- index.js
+++ index.js
@@ -1,41 +1,36 @@
-/*Задача
-На основі задачі масива users створити новий масив users.
-При цьому об'єкти в новому масиві мають бути у такому іигляді:
-    fullname:'Nataliia Yakovenko',
-    email: 'yakovenkonatali999@gmail.com',
- */
-const users = [{
-    name:'Nataliia',
-    lastName:'Yakovenko',
-    age: 41,
-    email: 'yakovenkonatali999@gmail.com',
-},{
-    name:'Roland',
-    lastName:'Simonyan',
-    age: 26,
-    email: 'roland@gmail.com',
-},{
-    name:'Bogdan',
-    lastName:'Horobriy',
-    age: 30,
-    email: 'horobriy@gmail.com',
-}]
+//argument - схожий на масив, але він НЕ є масивом
 
-//Варіант мій
-function changeValueInObject(user){  //функція буде приймати елемент об'єкта
-    return {                         // повертаємо новий об'єкт
-        fullname: `${user.name} ${user.lastName}`,
-        email: user.email
-    }
+function t(){
+    arguments[0]
+    arguments[1]
+    arguments[2]
+    arguments[3]= 'Nataliia'
+    console.log(arguments);
+
 }
+t(1,2,3,4,5,6,);
 
-const newUsers = users.map(changeValueInObject)
-console.log(newUsers);
-//----------------------------------------------------------------------------
-// Задача 2
-// на основі нашої задачі зменшити вік користувачів
+//console.log(t);
+console.dir(t) //console.dir - дозволяє більш розгорнуто передивитись функцію
 
-function ageDecrement(user){
-    user.age -=1;
+//будь-яка JS функція - є об'єкт
+//-------------------------------------------------------------------------
+//Приклад argument
+//функція, яка буде додавати два числа
+function sum (a,b){
+ console.log(arguments);
+ return a+b;
 }
-users.forEach(ageDecrement)
+//console.log(sum(3, 4));
+//--------------------------------------------------------------------------
+//Створити нормальний масив із об'єкта arguments
+//Написати функцію, яка повертає суму БУДЬ-ЯКОЇ кількості переданої в неї аргументів
+function sum (){
+const arrayArgs = Array.from(arguments)
+let sum = 0;
+for(let i = 0;i < arrayArgs.length; i++){
+    sum += arrayArgs[i];
+}
+return sum;
+   }
+   console.log(sum(1,2,3,4,5,6,7,8,9));
