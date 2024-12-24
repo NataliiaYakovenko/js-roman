0d0e97be8b625728c7d70df049f1099da442398b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-24 14:04:28 +0200

    Add prototype

--- index.js
+++ index.js
@@ -1,74 +1,27 @@
-// Створюємо власний масив
+//Prototype - прототипи масиву
 
-/*function MyArray(){
-    this.length = 0; //масив має свою довжину
+const cat = {              // об'єкт кота
+    name: 'Barsik',
+    color: 'red',
+    age: 1
+}
 
-    this.push = function(value){ //значення яке потрібно додати до масиву
-          // значення value потрібно додати в КІНЕЦЬ масиву
-     this[this.length] = value;
-     this.length++;
-     return this.length;     
-    }
-};
-
-const array = new MyArray();
-array.push(1);
-array.push(2);
-//-----------------------------------------------------------------------------------
-
-// перепишіть push таким чином щоб push міг приймати та додавав до масиву будь-яку кількість переданих елементів
-function MyArray(){
-    this.length = 0; 
-    
-    this.push = function(value){ 
-        for(let i =0; i < arguments.length; i++){
-            this[this.length] = arguments[i];
-            this.length++;
-        }
-        return this.length;     
+const cat2 = {              // об'єкт кота2
+    name: 'Murzik',
+    color: 'black',
+    age: 5
+}
+// прототип
+const catMethods = {          // методи кота
+    run: function(){
+        console.log('Cat is running');
+    },
+    myau: function(){
+        console.log('Myau');
     }
 }
-const array = new MyArray();
-array.push(1);
-array.push(2);
-array.push(3,4,77,44,59,5)*/
-//-----------------------------------------------------------------------------
-// Видалити останній елемент
-
-/*this.pop = function(){
-if(this.length > 0){
-    //1 Зберегти останні елемент
-    const lastItem = this[this.length - 1];
-    //2 Видалити останній елемент із масиву
-    delete this[this.length - 1];
-    //3 Зменшити довжину масиву на 1
-    this.length--;
-    //4 Повернути останній елемент
-    return lastItem;
-} else {
-    return undefined;
-}
-}
-const array = new MyArray();
-array.push(1);
-array.push(2);
-array.push(3,4,77,44,59,5);*/
-//----------------------------------------------------------------------
-
-this.forEach = function(callback){
- for(let i = 0; i < this.length;i++){
-    callback(this[i], i, this);
-    //this[i] - поточний переглядаємий елементи масива
-    //і - індекс цього елементу у маиві
-    //this - посилання на поточний масив
- }
-}
-const array = new MyArray();
-array.push(1);
-array.push(2);
-array.push(3,4,77)
-
-//Відконсольлогувати квадрати кожного числа в масив array
- array.forEach((item)=>{
-    console.log(item**2);//item виведений у квадрат
- })
+//Потрібно прикрепити catMethods до cat
+//Свойство cat яке вказує на прототип об'єкта
+//cat.__proto__  //cat.__(це два підкреслення)proto__(це два підкреслення)
+cat.__proto__ = catMethods  //прототипне посилання
+cat2.__proto__ = catMethods //прототипне посилання для кота2
