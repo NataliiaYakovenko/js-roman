77ce8a71d123b508f17a377b2e97da6bc6a6538f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2024-12-29 14:19:25 +0200

    Add Задачі перевірити на заборонені слова, перевірити на паліндром

--- index.js
+++ index.js
@@ -1,24 +1,81 @@
-//Задача1
-//Написати функцію, яка приймає рядок, 
-//в якому кожне слово повинно починається в великої літери
 
-function getCapitalLetter(string){
-  //Розбиваємо рядок на окремі слова
-  let words = string.split(' ')
-  console.log(words);
-  //Циклом проходимо по кожному слову і робтмо першу літеру великою
-  for(let i = 0; i < words.length;i++){
-    //перезаписуємо масив//words[i], words - наш масив,[i] - поточна позиція циклу.
-    words[i]=words[i].charAt(0).toUpperCase()+words[i].slice(1);
-                          // words[i] - отримуємо поточне переглядаєме слово
-                          //charAt(0) - отримуємо доступ до першої літери кожного слова
-                          //.toUpperCase() - робить літери великими
-                          //words[i] - беремо поточне переглядаєме слово
-                          //.slice(1) - прикліюємо кожне слово з другої літери            
-  }
-    return console.log(words.join(' '));//переобразовуємо масив в строку(не забуваєио між лапками пробіл)
+/*Задача 1
+Написати функцію checkSpam, яка повертає true,
+якщо переданий рядок містить слова 'xxx'або 'viagra'.
+Якщо заборонених слів у рядку не має повертнути falce
 
+checkSpam('buy ViAgRa now') //true
+checkSpam('free xxxxxxxxxxxx') //true
+checkSpam('innocent rabbit') //falce
+ */
+//variant1
+function checkSpam(string){
+   const stringLowerCase = string.toLowerCase()
+   //console.log(stringLowerCase)
+if(stringLowerCase.includes('xxx')|| stringLowerCase.includes('viagra')){
+  return true;
+} else{
+  return false;
 }
 
-getCapitalLetter('nataliia is beautiful girl! she lives in zaporizhzha.')
-getCapitalLetter('It is snow outside')
+}
+console.log(checkSpam('buy ViAgRa now'));
+console.log(checkSpam('free xxxxxxxxxxxx'));
+console.log(checkSpam('innocent rabbit'));
+
+
+//variant2
+function checkSpam(string){
+  const stringLowerCase = string.toLowerCase()
+  return stringLowerCase.includes('xxx')|| stringLowerCase.includes('viagra')
+ 
+}
+console.log(checkSpam('buy ViAgRa now'));
+console.log(checkSpam('free xxxxxxxxxxxx'));
+console.log(checkSpam('innocent rabbit'));
+
+
+//variant3
+function checkSpam(string){
+  const array = ['ViAgRa','xxx','drugs']
+  for(let i = 0;i < array.length;i++)
+  if(string.toLowerCase().includes(array[i])){
+     return true
+  }
+  return false;
+}
+console.log(checkSpam('buy ViAgRa now'));
+console.log(checkSpam('free xxxxxxxxxxxx'));
+console.log(checkSpam('innocent rabbit'));
+
+
+
+//-----------------------------------------------------------------------------------------
+/*Задача 2
+Написати функцію, яка перевіряє чи є переданий їй рядок - паліндромом, не зважаючі на регістр
+паліндром - це коли рядок з обох сторін читається однаково
+
+ */
+
+/*Алгоритм рішення
+  1. Приймаємо строку від користувача
+  2. Приводимо строку до нижнього регістру
+  3. Перевернути строку 
+  4. Перевернуту строку перевірити з оригінальною строкою
+ */
+function isPalindrom(string){
+  const newString = string.toLowerCase();
+  console.log(newString)
+  
+  const arrayString = newString.split('').reverse().join('')
+  console.log(arrayString);
+  
+  const result = newString === arrayString;
+  return result
+}
+
+
+
+console.log(isPalindrom('Anna'));
+console.log(isPalindrom('Mama'));
+console.log(isPalindrom('Barab'));
