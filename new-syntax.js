28504c1bd5137d378414623cc26da1578b7bdcf1
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-02 14:58:23 +0200

    Add практика параметри за замовченням

--- new-syntax.js
+++ new-syntax.js
@@ -1,16 +1,13 @@
-//Задача
-/*
-Напичати клас Worker.
-У працівника є: ім'я, прізвище, ставка за один робочий день,
- кількість відпрацьованих днів у цьому місяці
- Метод, який повертає зарплату цього працівника за поточний місяць
-*/
+
+const MIN_ZP = 7100;
+const WORK_DAYS = 21;
+const MIN_RATE = MIN_ZP / WORK_DAYS; //MIN_RATE мінімальна ставка
 
 class Worker{
-  constructor(name,lastname,salary1Day,daysOfMonth){
+  constructor(name,lastname,salary1Day = MIN_RATE,daysOfMonth = WORK_DAYS){
     this.name = name;
     this.lastname = lastname;
-    this.salary1Day = salary1Day;
+    this.salary1Day = Number(salary1Day.toFixed(2));//зробити округлення до 2 знаків пісял коми/Number робить суму числом
     this.daysOfMonth = daysOfMonth;
   }
   getSalaryCurrentMonth(){
@@ -18,8 +15,19 @@ class Worker{
   }
 }
 
-const worker1 = new Worker('Nataliia','Yakovenko',1200, 24)
+const worker1 = new Worker('Nataliia','Yakovenko',1200, 21)
 console.log(worker1)
 console.log(worker1.getSalaryCurrentMonth());
 
+const worker2 = new Worker('Lidia','Yakovenko')
+console.log(worker2)
+console.log(worker2.getSalaryCurrentMonth());
+//------------------------------------------------------------------------------
+//Параметри за замовченням
 
+function sum(a =10,b =5){
+  return a+b;
+}
+console.log(sum()); //15
+console.log(sum(3,3)); //6
+console.log(sum(3)); //8
