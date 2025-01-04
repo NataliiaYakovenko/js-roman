59273b5c55991e6235ddd800fd8299ce9498f442
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-04 07:14:21 +0200

    Add class задача авто+паливо

--- new-syntax.js
+++ new-syntax.js
@@ -1,33 +1,36 @@
 
-const MIN_ZP = 7100;
-const WORK_DAYS = 21;
-const MIN_RATE = MIN_ZP / WORK_DAYS; //MIN_RATE мінімальна ставка
+/*Задача
+є клач авто
+є клас паливо
 
-class Worker{
-  constructor(name,lastname,salary1Day = MIN_RATE,daysOfMonth = WORK_DAYS){
-    this.name = name;
-    this.lastname = lastname;
-    this.salary1Day = Number(salary1Day.toFixed(2));//зробити округлення до 2 знаків пісял коми/Number робить суму числом
-    this.daysOfMonth = daysOfMonth;
+Порахувати загальну вагу автомобіля (вага авто + вага палива)
+об'єм * шільність
+*/
+class Fuil{
+  constructor(volume,density){
+     this.volume = volume;
+     this.density = density;
   }
-  getSalaryCurrentMonth(){
-     return this.salary1Day * this.daysOfMonth;
+  getWeight(){
+    return this.volume * this.density;
   }
 }
+const benzin = new Fuil(50,0.9)
+console.log(benzin);
+console.log(benzin.getWeight());
 
-const worker1 = new Worker('Nataliia','Yakovenko',1200, 21)
-console.log(worker1)
-console.log(worker1.getSalaryCurrentMonth());
-
-const worker2 = new Worker('Lidia','Yakovenko')
-console.log(worker2)
-console.log(worker2.getSalaryCurrentMonth());
-//------------------------------------------------------------------------------
-//Параметри за замовченням
-
-function sum(a =10,b =5){
-  return a+b;
+class Avto{
+  constructor(brand,owenWeight, fuil){
+      this.brand = brand;
+      this.owenWeight = owenWeight;
+      this.fuil = fuil;
+  }
+  //метод, який обчислює повну вагу
+  //його власна вагаo wenWeight + вага палива
+  getFullWeght(){
+    return this.owenWeight + this.fuil.getWeight();
+  }
 }
-console.log(sum()); //15
-console.log(sum(3,3)); //6
-console.log(sum(3)); //8
+const car = new Avto('Toyota',4000,benzin)
+console.log(car)
+console.log(car.getFullWeght());
