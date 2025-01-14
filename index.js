cc5932a37bc2e78aabde1aa060774224b1687a7e
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-14 22:16:52 +0200

    Add Черга

--- index.js
+++ index.js
@@ -1,85 +1,52 @@
-//Стек Stack
-
-//Принцип LIFO
-//last in - first out
-
-//Stack overflow - помилка виникає при переповнені стеку
-
+//Queue - черга
 /*
-Stack - має реалізовувати такі операці
-- push - вставляємо елемент на вершину стеку
-- pop - видалення елементу з вершини стеку і повернення видаленого елементу
-- isEmpty - повертається true,якщо стек пустий//false - якщо стек не пустий
-- pick - коли нам повертає останній елемент, але нічого не робить з ним і не видаляє із стеку
-
+Методи queue
+1. Enqueue - вставляє елемент в кінець черги
+2. Dequeue - видаляє елемент з початку черги
 */
-//Реалізація стека
-class Stack {
-  constructor(maxSize) {
-    this._maxSize = maxSize;
-    this._size = 0;
-  }
-
-  get isEmpty() {
-    return this._size === 0; // якщо розмір = 0 тоді повертається true/ (оператор === повертає булєвоє значення)
-  }
-  set size(newValue) {
-    throw new Error("Access denied"); // доступ заборонено
+//Створення черги
+class Queue {
+  constructor(head, tail) {
+    this._head = 0;
+    this._tail = 0;
   }
   get size() {
-    return this._size;
+    return this._tail - this._head;
   }
-  set maxSize(newValue) {
-    throw new Error("Access denied"); // доступ заборонено
-  }
-  get maxSize() {
-    return this._maxSize;
-  }
-  push(newValue) {
-    if (this._size > this._maxSize) {
-      throw new Error("Stack overflow");
-    }
-    this[`_${this.size}`] = newValue;
-    this._size++;
+
+  enqueue(value) {
+    //tail відповідає на запитання, який елемент має бути останнім
+    this[this._tail] = value;
+    this._tail++; //на 1 збільшуємо наш tail
     return this.size;
   }
-  pop() {
-    if (this.size > 0);
-    {
-      const lastitem = this[`_${this.size - 1}`];
-      delete this[`_${this.size}`];
-      this._size--;
 
-      return lastitem;
+  dequeue() {
+    if (this.size > 0) {
+      const firstItem = this[this._head]; //це і буде елемент, який стоїть на початку черги
+      delete this[this._head]; //видаляємо елемент що стоїть на початку черги
+      this._head++;
+      return firstItem;
+    } else{
+      return undefined;
     }
   }
-  pick() {
-    return this[`_${this.size-1}`];
-  }
 }
-const myStack = new Stack(4)
-console.log(myStack)
-console.log(myStack.push('task1'));
-console.log(myStack)
-console.log(myStack.isEmpty)
-console.log(myStack.push('task2'));
-console.log(myStack)
-console.log(myStack.push('task3'));
-console.log(myStack)
-console.log(myStack.pick())
-console.log(myStack.push('task4'));
-console.log(myStack)
-console.log(myStack.pick())
-console.log(myStack.pop())
-console.log(myStack)
-console.log(myStack.pick())
-console.log(myStack.pop())
-console.log(myStack)
-console.log(myStack.pick())
-console.log(myStack.pop())
-console.log(myStack)
-console.log(myStack.pick())
-console.log(myStack.pop())
-console.log(myStack)
-console.log(myStack.isEmpty)
-
+const queue = new Queue()
+console.log(queue)
+console.log(queue.enqueue('task1'))
+console.log(queue)
+console.log(queue.enqueue('task2'))
+console.log(queue)
+console.log(queue.enqueue('task3'))
+console.log(queue)
+console.log(queue.dequeue())
+console.log(queue)
+console.log(queue.dequeue())
+console.log(queue)
+console.log(queue.dequeue())
+console.log(queue)
+console.log(queue.dequeue())
+console.log(queue)
+console.log(queue.size)
+console.log(queue.dequeue())
