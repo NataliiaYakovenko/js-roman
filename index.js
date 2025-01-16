83375cfee96901bd17a90ce91a8aa8ea4b0edd35
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-16 22:48:01 +0200

    Add Методи МАР

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
-//Задача МАР
-/*
-є два об'єкти users
-сonst user1 = {
-firstName: 'Alex',
-lastName: 'Doe',
-id: 1,
+// Map - методи продовження
+//Конструктор new Map() - очикує від нас ітерірований об'єкт
+//Картеж  <--[['key1', 'value1'],['key2','value2']], коли в основному масиві є пар масивів
 
-сonst user1 = {
-firstName: 'John',
-lastName: 'Doe',
-id: 2,
-}
+const map1 = new Map([['key1', 'value1'],['key2','value2']])
+console.log(map1);
 
-const johnMessages = ['Hello', 'How are you?']
-const alexMessages = ['Hi!', 'I am fine']
+//метод  set() може перезаписувати значення
+map1.set('key2','value7');
+console.log(map1);  //Map(2) {'key1' => 'value1', 'key2' => 'value7'}
+//---------------------------------------------------------------
 
-Задача. За допомогою Мар зв'язати користувача з його повідомленнями
-щоб за id користувача можна було знайти його повідомлення 
+//Метод map.prototype.entries() - метод повертає ітератор. 
+//Ітератор повертає пари (ключ, значення) для кожного елемента
+console.log(map1.entries()); //MapIterator {'key1' => 'value1', 'key2' => 'value2'}
 
-*/
-const user1 = {
-    firstName: 'Alex',
-    lastName: 'Doe',
-    id: 1,
-} 
-    const user2 = {
-    firstName: 'John',
-    lastName: 'Doe',
-    id: 2,
-    }
-    
-    const johnMessages = ['Hello', 'How are you?']
-    const alexMessages = ['Hi!', 'I am fine']
+const entriesIterator = map1.entries()
+console.log(entriesIterator);
+//--------------------------------------------------------------
 
-    const getMessage = new Map();
-    getMessage.set(user1.id,johnMessages);
-    getMessage.set(user2.id,alexMessages);
+//Метод map.next() - виведення ключів і значень
+// після виведення нвступного map.next() отримуємо наступний ключ і значення
+console.log(entriesIterator.next()); //'key1' => 'value1'
+console.log(entriesIterator.next())  //'key2' => 'value2'
+console.log(entriesIterator.next())   //{value: undefined, done: true}
+//----------------------------------------------------------------
 
-    console.log(getMessage.get(user1.id))
-    console.log(getMessage.get(user2.id))
+//Метод map.keys() - повертає ітератор з усіма ключами з об'єкту map
+const keysIterator = map1.keys()
+console.log(keysIterator) //MapIterator {'key1', 'key2'}
+console.log(keysIterator.next()) //{value: 'key1', done: false}
+//-----------------------------------------------------------------
+
+//Метод map.values() -повертає ітератор з усіма значеннями з об'єкту map
+const valuesIterator = map1.values()
+console.log(valuesIterator); //MapIterator {'value1', 'value7'}
+//------------------------------------------------------------------
+
+//Перетворюємо map в масив
+const arrayMap = Array.from(map1)
+console.log(arrayMap)
+//-------------------------------------------------------------------
+
+//Метод map.forEach() - використовується для iтерації (перебору) всіх елементів у МАР
+// і виклику певної функції(callback) для кожного елемента нашої мар
+//map.forEach() працює также, як forEach() в масиві
+//Задача: вивести на консоль мар
+map1.forEach((value,key,map)=>{
+    console.log(`${key} - ${value}`);
+})
+//--------------------------------------------------------------------
+
+//Метод map.delete() - використовується для видалення пари ключ-значення з об'єкта Мар
+//відповідно до якогось ключа
+console.log(map1.has('key2'))
+//console.log(map1.delete('key2'));
+console.log(map1)  //Map(1) {'key1' => 'value1'}
+//-------------------------------------------------------------------
+
+//Метод map.clear() - використовується для повного очищення об'єкта мар
+//видаляючи всі пари ключ-значення, які знаходяться там
+console.log(map1.size);
+//console.log(map1.clear()); //Map(0) {size: 0}
+console.log(map1) 
+//--------------------------------------------------------------------
+
+//Задача: вивести на консоль мар
+console.log(...map1)
+console.log(...map1.keys())
+console.log(...map1.values())
