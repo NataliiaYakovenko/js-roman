ce2c2d1a71647948733cce1154ab9309ce120e09
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-18 21:43:12 +0200

    Add Приклади замикання

--- index.js
+++ index.js
@@ -56,15 +56,34 @@ function makeCounter2() {
     },
   };
 }
-const fnObj = makeCounter2()
-console.log(fnObj);  //{increment: ƒ, dicrement: ƒ}
+const fnObj = makeCounter2();
+console.log(fnObj); //{increment: ƒ, dicrement: ƒ}
 
-console.log(fnObj.increment())  //1
-console.log(fnObj.increment())  //2
-console.log(fnObj.increment())  //3
+console.log(fnObj.increment()); //1
+console.log(fnObj.increment()); //2
+console.log(fnObj.increment()); //3
 
-console.log(fnObj.dicrement())  //2
-console.log(fnObj.dicrement())  //1
-console.log(fnObj.dicrement())  //0
-
+console.log(fnObj.dicrement()); //2
+console.log(fnObj.dicrement()); //1
+console.log(fnObj.dicrement()); //0
+//------------------------------------------------------
 //Зробіть методи, які будуть збільшувати,зменшувати counter на певну кількість
+
+function makeCounter3() {
+  let counter = 0;
+
+  return {
+    increment(amount = 1) {
+      return (counter += amount);
+    },
+    decrement(amount = 1) {
+      return (counter -= amount);
+    },
+  };
+}
+const fnObj3 = makeCounter3();
+console.log(fnObj3.increment(2));
+console.log(fnObj3.decrement(1));
+console.log(fnObj3.increment(3));
+console.log(fnObj3.decrement(1));
+
